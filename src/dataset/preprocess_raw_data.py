c3d6386947182c105fe56dbb083dde550454e874
Mukul Joshi <joshimukul29@gmail.com>
2020-02-09 23:54:03 +0530

    renamed previously called processed files as preprocessed or interim files

--- src/dataset/preprocess_raw_data.py
+++ src/dataset/preprocess_raw_data.py
@@ -8,13 +8,13 @@ project_dir = os.path.abspath('../..')
 raw_data_dir = os.path.join(project_dir, 'data', 'raw')
 
 # Processed data directory, blank lines are removed and the data is stored in the form of a csv file
-processed_data_dir = os.path.join(project_dir, 'data', 'processed')
+interim_data_dir = os.path.join(project_dir, 'data', 'interim')
 
 ################################## FOR QWS1 #########################################
 
-# open both files for qws1 dataset (raw for reading and processed for writing
+# open both files for qws1 dataset (raw for reading and interim for writing
 qws1_raw_file = open(os.path.join(raw_data_dir, "qws1.txt"), "r")
-qws1_processed_file = open(os.path.join(processed_data_dir, "qws1_processed.csv"), "w")
+qws1_interim_file = open(os.path.join(interim_data_dir, "qws1_interim.csv"), "w")
 
 # regex for blank lines or lines beginning with a "#"
 regex_filter = re.compile(r'(^\s*$)|(^#)')
@@ -22,19 +22,19 @@ regex_filter = re.compile(r'(^\s*$)|(^#)')
 # read all lines and store them in a list
 qws1_raw_list = qws1_raw_file.readlines()
 
-# filter the data and write it in the processed file
+# filter the data and write it in the interim file
 filtered_data = filter(lambda i: not regex_filter.search(i), qws1_raw_list)
-qws1_processed_file.writelines(filtered_data)
+qws1_interim_file.writelines(filtered_data)
 
 # close both files
 qws1_raw_file.close()
-qws1_processed_file.close()
+qws1_interim_file.close()
 #####################################################################################
 
 ################################## FOR QWS2 #########################################
 # open both files for qws2 dataset
 qws2_raw_file = open(os.path.join(raw_data_dir, "qws2.txt"), "r")
-qws2_processed_file = open(os.path.join(processed_data_dir, "qws2_processed.csv"), "w")
+qws2_interim_file = open(os.path.join(interim_data_dir, "qws2_interim.csv"), "w")
 
 # regex for blank lines or lines beginning with a "#"
 regex_filter = re.compile(r'(^\s*$)|(^#)')
@@ -42,11 +42,11 @@ regex_filter = re.compile(r'(^\s*$)|(^#)')
 # read all lines and store them in a list
 qws2_raw_list = qws2_raw_file.readlines()
 
-# filter the data and write it in the processed file
+# filter the data and write it in the interim file
 filtered_data = filter(lambda i: not regex_filter.search(i), qws2_raw_list)
-qws2_processed_file.writelines(filtered_data)
+qws2_interim_file.writelines(filtered_data)
 
 # close both files
 qws2_raw_file.close()
-qws2_processed_file.close()
+qws2_interim_file.close()
 #####################################################################################
