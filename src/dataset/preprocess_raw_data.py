48e2dc218358fe9abc1a2b10398fa8d2b4fe51da
Mukul Joshi <joshimukul29@gmail.com>
2020-02-19 05:06:05 +0530

    Added a function around the code

--- src/dataset/preprocess_raw_data.py
+++ src/dataset/preprocess_raw_data.py
@@ -1,52 +1,50 @@
-import os
+import os.path
 import re
+import paths
 
-# Main project directory
-project_dir = os.path.abspath('../..')
 
-# Raw data directory, data in this directory is treated as immutable
-raw_data_dir = os.path.join(project_dir, 'data', 'raw')
+def preprocess_raw_data():
+    ################################# FOR QWS1 #########################################
 
-# Processed data directory, blank lines are removed and the data is stored in the form of a csv file
-interim_data_dir = os.path.join(project_dir, 'data', 'interim')
+    # open both files for qws1 dataset (raw for reading and interim for writing
+    qws1_raw_file = open(os.path.join(paths.raw_data_dir, "qws1.txt"), "r")
+    qws1_interim_file = open(os.path.join(paths.interim_data_dir, "qws1_interim.csv"), "w")
 
-################################## FOR QWS1 #########################################
+    # regex for blank lines or lines beginning with a "#"
+    regex_filter = re.compile(r'(^\s*$)|(^#)')
 
-# open both files for qws1 dataset (raw for reading and interim for writing
-qws1_raw_file = open(os.path.join(raw_data_dir, "qws1.txt"), "r")
-qws1_interim_file = open(os.path.join(interim_data_dir, "qws1_interim.csv"), "w")
+    # read all lines and store them in a list
+    qws1_raw_list = qws1_raw_file.readlines()
 
-# regex for blank lines or lines beginning with a "#"
-regex_filter = re.compile(r'(^\s*$)|(^#)')
+    # filter the data and write it in the interim file
+    filtered_data = filter(lambda i: not regex_filter.search(i), qws1_raw_list)
+    qws1_interim_file.writelines(filtered_data)
 
-# read all lines and store them in a list
-qws1_raw_list = qws1_raw_file.readlines()
+    # close both files
+    qws1_raw_file.close()
+    qws1_interim_file.close()
+    #####################################################################################
 
-# filter the data and write it in the interim file
-filtered_data = filter(lambda i: not regex_filter.search(i), qws1_raw_list)
-qws1_interim_file.writelines(filtered_data)
+    ################################## FOR QWS2 #########################################
+    # open both files for qws2 dataset
+    qws2_raw_file = open(os.path.join(paths.raw_data_dir, "qws2.txt"), "r")
+    qws2_interim_file = open(os.path.join(paths.interim_data_dir, "qws2_interim.csv"), "w")
 
-# close both files
-qws1_raw_file.close()
-qws1_interim_file.close()
-#####################################################################################
+    # regex for blank lines or lines beginning with a "#"
+    regex_filter = re.compile(r'(^\s*$)|(^#)')
 
-################################## FOR QWS2 #########################################
-# open both files for qws2 dataset
-qws2_raw_file = open(os.path.join(raw_data_dir, "qws2.txt"), "r")
-qws2_interim_file = open(os.path.join(interim_data_dir, "qws2_interim.csv"), "w")
+    # read all lines and store them in a list
+    qws2_raw_list = qws2_raw_file.readlines()
 
-# regex for blank lines or lines beginning with a "#"
-regex_filter = re.compile(r'(^\s*$)|(^#)')
+    # filter the data and write it in the interim file
+    filtered_data = filter(lambda i: not regex_filter.search(i), qws2_raw_list)
+    qws2_interim_file.writelines(filtered_data)
 
-# read all lines and store them in a list
-qws2_raw_list = qws2_raw_file.readlines()
+    # close both files
+    qws2_raw_file.close()
+    qws2_interim_file.close()
+    #####################################################################################
 
-# filter the data and write it in the interim file
-filtered_data = filter(lambda i: not regex_filter.search(i), qws2_raw_list)
-qws2_interim_file.writelines(filtered_data)
 
-# close both files
-qws2_raw_file.close()
-qws2_interim_file.close()
-#####################################################################################
+if __name__ == "__main__":
+    preprocess_raw_data()
