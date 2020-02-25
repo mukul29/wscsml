d49e042edd408ae8ebc66b97824545332abf47cf
Mukul Joshi <joshimukul29@gmail.com>
2020-02-26 00:52:59 +0530

    Generalized the preprocessing steps for removing comments, blank lines from the raw dataset

--- src/dataset/preprocess_raw_data.py
+++ src/dataset/preprocess_raw_data.py
@@ -3,48 +3,39 @@ import re
 import paths
 
 
-def preprocess_raw_data():
-    ################################# FOR QWS1 #########################################
-
-    # open both files for qws1 dataset (raw for reading and interim for writing
-    qws1_raw_file = open(os.path.join(paths.raw_data_dir, "qws1.txt"), "r")
-    qws1_interim_file = open(os.path.join(paths.interim_data_dir, "qws1_interim.csv"), "w")
-
+def preprocess_raw_data(qws_raw_file, qws_interim_file):
+    """
+    This function takes the raw dataset file and removes blank lines and comments and storing it as interim an dataset
+    :param qws_raw_file: file object(class '_io.TextIOWrapper' for the raw file
+    :param qws_interim_file: file object(class '_io.TextIOWrapper' for the interim file
+    :return: None
+    """
     # regex for blank lines or lines beginning with a "#"
     regex_filter = re.compile(r'(^\s*$)|(^#)')
 
     # read all lines and store them in a list
-    qws1_raw_list = qws1_raw_file.readlines()
+    qws1_raw_list = qws_raw_file.readlines()
 
     # filter the data and write it in the interim file
     filtered_data = filter(lambda i: not regex_filter.search(i), qws1_raw_list)
-    qws1_interim_file.writelines(filtered_data)
+    qws_interim_file.writelines(filtered_data)
 
-    # close both files
-    qws1_raw_file.close()
-    qws1_interim_file.close()
-    #####################################################################################
-
-    ################################## FOR QWS2 #########################################
-    # open both files for qws2 dataset
-    qws2_raw_file = open(os.path.join(paths.raw_data_dir, "qws2.txt"), "r")
-    qws2_interim_file = open(os.path.join(paths.interim_data_dir, "qws2_interim.csv"), "w")
-
-    # regex for blank lines or lines beginning with a "#"
-    regex_filter = re.compile(r'(^\s*$)|(^#)')
-
-    # read all lines and store them in a list
-    qws2_raw_list = qws2_raw_file.readlines()
-
-    # filter the data and write it in the interim file
-    filtered_data = filter(lambda i: not regex_filter.search(i), qws2_raw_list)
-    qws2_interim_file.writelines(filtered_data)
-
-    # close both files
-    qws2_raw_file.close()
-    qws2_interim_file.close()
-    #####################################################################################
+    qws_raw_file.close()
+    qws_interim_file.close()
 
 
 if __name__ == "__main__":
-    preprocess_raw_data()
+    # open both files for qws1 dataset (raw for reading and interim for writing
+    qws1_raw_file = open(os.path.join(paths.raw_data_dir, "qws1.txt"), "r")
+    qws1_interim_file = open(os.path.join(paths.interim_data_dir, "qws1_interim.csv"), "w")
+    preprocess_raw_data(qws1_raw_file, qws1_interim_file)
+    # close both files
+    qws1_raw_file.close()
+    qws1_interim_file.close()
+
+    # open both files for qws1 dataset (raw for reading and interim for writing
+    qws2_raw_file = open(os.path.join(paths.raw_data_dir, "qws2.txt"), "r")
+    qws2_interim_file = open(os.path.join(paths.interim_data_dir, "qws2_interim.csv"), "w")
+    # close both files
+    qws2_raw_file.close()
+    qws2_interim_file.close()
