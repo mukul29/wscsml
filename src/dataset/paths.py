95824a6029b4e0eb0e2edef876bf9619c791ecc3
Mukul Joshi <joshimukul29@gmail.com>
2020-03-05 22:59:36 +0530

    Added path for the processed data directory

--- src/dataset/paths.py
+++ src/dataset/paths.py
@@ -7,6 +7,7 @@ project_dir = os.path.abspath(os.path.join(source_dir, os.pardir))
 
 # Raw data directory, data in this directory is treated as immutable
 raw_data_dir = os.path.join(project_dir, 'data', 'raw')
-
-# Processed data directory, blank lines are removed and the data is stored in the form of a csv file
+# Interim data directory, blank lines are removed and the data is stored in the form of a csv file
 interim_data_dir = os.path.join(project_dir, 'data', 'interim')
+# Processed data directory, new qws values are used and new classes as well
+processed_data_dir = os.path.join(project_dir, 'data', 'processed')
