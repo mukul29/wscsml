bce023bdbe4a1f7a052f0ff4b704e368966ab38a
Mukul Joshi <joshimukul29@gmail.com>
2020-03-05 22:59:09 +0530

    Created processed dataset in the data/processed directory to be further used for classification

--- src/dataset/make_dataset.py
+++ src/dataset/make_dataset.py
@@ -2,9 +2,10 @@ import paths
 import os.path
 import compute_wsrf
 import numpy as np
+import pandas as pd
 
 
-def make_dataset(interim_file_path, processed_file_path, weights):
+def make_dataset(interim_file_path, processed_file_path, weights, version):
     """
     This function is used to make the final dataset to be used, computes and adds the new WsRF and service classification
     :param interim_file_path: str
@@ -13,6 +14,8 @@ def make_dataset(interim_file_path, processed_file_path, weights):
         string containing the path of the processed file to be stored
     :param weights:
         floating point numbers representing weights of each attribute which add up to 1.0
+    :param: version:
+        integer (1 or 2) for qws version 1 or 2 respectively
     :return: None
     """
     qws_wsrf, qws_complete_numpy_array = compute_wsrf.compute_wsrf(interim_file_path, weights)
@@ -30,15 +33,22 @@ def make_dataset(interim_file_path, processed_file_path, weights):
         score = np.append(score, level)
         qws_wsrf_level = np.append(qws_wsrf_level, score)
     qws_wsrf_level = qws_wsrf_level.reshape(qws_wsrf.shape[0], 2)
-    print(qws_wsrf_level)
+    if(version == 1):
+        qws_complete_numpy_array[:, 9:11] = qws_wsrf_level
+    elif(version == 2):
+        qws_complete_numpy_array = np.hstack((qws_complete_numpy_array, np.zeros((qws_wsrf.shape[0], 2))))
+        qws_complete_numpy_array[:, 11:13] = qws_complete_numpy_array[:, 9:11]
+        qws_complete_numpy_array[:, 9:11] = qws_wsrf_level
+    else:
+        print("Version has to be either 1 or 2")
 
-
-
-
+    qws_complete_dataframe_new = pd.DataFrame(qws_complete_numpy_array)
+    qws_complete_dataframe_new = qws_complete_dataframe_new.astype({10: int})
+    qws_complete_dataframe_new.to_csv(processed_file_path, header=False, index=False)
 
 
 if __name__ == "__main__":
     weights = [0.20, 0.17, 0.18, 0.10, 0.10, 0.10, 0.00, 0.05, 0.10]
-    interim_file_path = os.path.join(paths.interim_data_dir, "qws2_interim.csv")
-    processed_file_path = os.path.join(paths.interim_data_dir, "qws2_processed.csv")
-    make_dataset(interim_file_path, processed_file_path, weights)
+    interim_file_path = os.path.join(paths.interim_data_dir, "qws1_interim.csv")
+    processed_file_path = os.path.join(paths.processed_data_dir, "qws1_processed.csv")
+    make_dataset(interim_file_path, processed_file_path, weights, 1)
